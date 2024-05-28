8145b28027c5a6ca95eee6e3e4cbba6845054a48
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2024-05-28 17:52:12 -0300

    feat: `LDA (indirect),y` instruction added

--- src/cpu.rs
+++ src/cpu.rs
@@ -26,6 +26,7 @@ const OPCODE_LDA_ABS: u8 = 0xAD;
 const OPCODE_LDA_ABX: u8 = 0xBD;
 const OPCODE_LDA_ABY: u8 = 0xB9;
 const OPCODE_LDA_IDX: u8 = 0xA1;
+const OPCODE_LDA_IDY: u8 = 0xB1;
 
 // ==================== OPCODES END =====================
 
@@ -75,6 +76,7 @@ impl CPU {
             OPCODE_LDA_ABX => self.lda_absolute_x(),
             OPCODE_LDA_ABY => self.lda_absolute_y(),
             OPCODE_LDA_IDX => self.lda_indirect_x(),
+            OPCODE_LDA_IDY => self.lda_indirect_y(),
             _ => panic!("invalid opcode: {:#X}", opcode),
         }
     }
@@ -105,6 +107,14 @@ impl CPU {
         let addr_l = self.fetch_byte() as u16;
         let addr_h = self.fetch_byte() as u16;
         (addr_h << 8) | addr_l
+    }
+
+    /// gets an addr using the value in `low` as the low byte
+    /// and in `high` as the high byte of the addr, in 2 cycles
+    fn read_addr(&mut self, low: u16, high: u16) -> u16 {
+        let addr_l = self.read_byte(low);
+        let addr_h = self.read_byte(high);
+        (addr_h as u16) << 8 | addr_l as u16
     }
 
     /// pushes an addr to the stack, wrapping around when overflowing or
@@ -226,12 +236,23 @@ impl CPU {
     /// flags affected: N and Z
     fn lda_indirect_x(&mut self) {
         let zpg_addr = self.fetch_byte();
-        let eff_zpg_addr = zpg_addr.wrapping_add(self.x);
+        let addr = zpg_addr.wrapping_add(self.x);
         self.cycles += 1;
+        let eff_addr = self.read_addr(addr.into(), addr.wrapping_add(1).into());
+        self.acc = self.read_byte(eff_addr);
+        self.lda_set_status();
+    }
 
-        let eff_addr_l = self.read_byte(eff_zpg_addr.into());
-        let eff_addr_h = self.read_byte(eff_zpg_addr.wrapping_add(1).into());
-        let eff_addr = (eff_addr_h as u16) << 8 | eff_addr_l as u16;
+    /// bytes: 2
+    /// cycles: 5 (+1 if page crossed)
+    /// flags affected: N and Z
+    fn lda_indirect_y(&mut self) {
+        let zpg_addr = self.fetch_byte();
+        let addr = self.read_addr(zpg_addr.into(), zpg_addr.wrapping_add(1).into());
+        let eff_addr = addr.wrapping_add(self.y.into());
+        if Self::page_crossed(addr, eff_addr) {
+            self.cycles += 1;
+        }
         self.acc = self.read_byte(eff_addr);
         self.lda_set_status();
     }
@@ -689,7 +710,7 @@ mod tests {
         const X: u8 = 0xAC;
 
         let mut memory = Memory::new();
-        // first instruction
+
         memory.set(OPCODE_LDA_IDX, MEM_OFFSET);
         let zpg_addr = 0x08;
         memory.set(zpg_addr, MEM_OFFSET + 1);
@@ -698,7 +719,7 @@ mod tests {
         memory.set(0x32, addr_h.into());
         memory.set(0x22, addr_l.into());
         memory.set(0x42, 0x3222);
-        // second instruction
+
         memory.set(OPCODE_LDA_IDX, MEM_OFFSET + 2);
         let zpg_addr = 0x57;
         memory.set(zpg_addr, MEM_OFFSET + 3);
@@ -707,7 +728,7 @@ mod tests {
         memory.set(0x83, addr_h.into());
         memory.set(0x46, addr_l.into());
         memory.set(0x00, 0x8346);
-        // third instruction
+
         memory.set(OPCODE_LDA_IDX, MEM_OFFSET + 4);
         let zpg_addr = 0x69;
         memory.set(zpg_addr, MEM_OFFSET + 5);
@@ -720,6 +741,148 @@ mod tests {
         let mut cpu = CPU::new(memory);
         cpu.reset();
         cpu.x = X;
+
+        let init_pc = cpu.pc;
+        let init_cycles = cpu.cycles;
+        let opcode = cpu.fetch_byte();
+        cpu.execute(opcode);
+        assert_eq!(0x42, cpu.acc);
+        assert_eq!(cpu.pc - init_pc, BYTES);
+        assert_eq!(cpu.cycles - init_cycles, CYCLES);
+        assert_eq!(cpu.status, CPU_DEFAULT_STATUS);
+
+        let pc_after_first_exec = cpu.pc;
+        let cycles_after_first_exec = cpu.cycles;
+        let opcode = cpu.fetch_byte();
+        cpu.execute(opcode);
+        assert_eq!(0x00, cpu.acc);
+        assert_eq!(cpu.pc - pc_after_first_exec, BYTES);
+        assert_eq!(cpu.cycles - cycles_after_first_exec, CYCLES);
+        assert_eq!(cpu.status, CPU_DEFAULT_STATUS | CSF_ZERO);
+
+        let pc_after_second_exec = cpu.pc;
+        let cycles_after_second_exec = cpu.cycles;
+        let opcode = cpu.fetch_byte();
+        cpu.execute(opcode);
+        assert_eq!(0x80, cpu.acc);
+        assert_eq!(cpu.pc - pc_after_second_exec, BYTES);
+        assert_eq!(cpu.cycles - cycles_after_second_exec, CYCLES);
+        assert_eq!(cpu.status, CPU_DEFAULT_STATUS | CSF_NEGATIVE);
+    }
+
+    #[test]
+    fn lda_indirect_y_without_page_crossing_test() {
+        const BYTES: u16 = 2;
+        const CYCLES: u64 = 5;
+        const MEM_OFFSET: u16 = UNRESERVED_MEMORY_ADDR_START;
+        const Y: u8 = 0xAC;
+
+        let mut memory = Memory::new();
+
+        memory.set(OPCODE_LDA_IDY, MEM_OFFSET);
+        let zpg_addr = 0x08;
+        memory.set(zpg_addr, MEM_OFFSET + 1);
+        let addr_h = 0x32;
+        let addr_l = 0x22;
+        memory.set(addr_l, zpg_addr.into());
+        memory.set(addr_h, zpg_addr.wrapping_add(1).into());
+        let addr = ((addr_h as u16) << 8 | addr_l as u16).wrapping_add(Y.into());
+        memory.set(0x42, addr);
+
+        memory.set(OPCODE_LDA_IDY, MEM_OFFSET + 2);
+        let zpg_addr = 0x57;
+        memory.set(zpg_addr, MEM_OFFSET + 3);
+        let addr_h = 0x83;
+        let addr_l = 0x46;
+        memory.set(addr_l, zpg_addr.into());
+        memory.set(addr_h, zpg_addr.wrapping_add(1).into());
+        let addr = ((addr_h as u16) << 8 | addr_l as u16).wrapping_add(Y.into());
+        memory.set(0x00, addr);
+
+        memory.set(OPCODE_LDA_IDY, MEM_OFFSET + 4);
+        let zpg_addr = 0x69;
+        memory.set(zpg_addr, MEM_OFFSET + 5);
+        let addr_h = 0x77;
+        let addr_l = 0x13;
+        memory.set(addr_l, zpg_addr.into());
+        memory.set(addr_h, zpg_addr.wrapping_add(1).into());
+        let addr = ((addr_h as u16) << 8 | addr_l as u16).wrapping_add(Y.into());
+        memory.set(0x80, addr);
+
+        let mut cpu = CPU::new(memory);
+        cpu.reset();
+        cpu.y = Y;
+
+        let init_pc = cpu.pc;
+        let init_cycles = cpu.cycles;
+        let opcode = cpu.fetch_byte();
+        cpu.execute(opcode);
+        assert_eq!(0x42, cpu.acc);
+        assert_eq!(cpu.pc - init_pc, BYTES);
+        assert_eq!(cpu.cycles - init_cycles, CYCLES);
+        assert_eq!(cpu.status, CPU_DEFAULT_STATUS);
+
+        let pc_after_first_exec = cpu.pc;
+        let cycles_after_first_exec = cpu.cycles;
+        let opcode = cpu.fetch_byte();
+        cpu.execute(opcode);
+        assert_eq!(0x00, cpu.acc);
+        assert_eq!(cpu.pc - pc_after_first_exec, BYTES);
+        assert_eq!(cpu.cycles - cycles_after_first_exec, CYCLES);
+        assert_eq!(cpu.status, CPU_DEFAULT_STATUS | CSF_ZERO);
+
+        let pc_after_second_exec = cpu.pc;
+        let cycles_after_second_exec = cpu.cycles;
+        let opcode = cpu.fetch_byte();
+        cpu.execute(opcode);
+        assert_eq!(0x80, cpu.acc);
+        assert_eq!(cpu.pc - pc_after_second_exec, BYTES);
+        assert_eq!(cpu.cycles - cycles_after_second_exec, CYCLES);
+        assert_eq!(cpu.status, CPU_DEFAULT_STATUS | CSF_NEGATIVE);
+    }
+
+    #[test]
+    fn lda_indirect_y_with_page_crossing_test() {
+        const BYTES: u16 = 2;
+        const CYCLES: u64 = 6;
+        const MEM_OFFSET: u16 = UNRESERVED_MEMORY_ADDR_START;
+        const Y: u8 = 0xAC;
+
+        let mut memory = Memory::new();
+
+        memory.set(OPCODE_LDA_IDY, MEM_OFFSET);
+        let zpg_addr = 0x08;
+        memory.set(zpg_addr, MEM_OFFSET + 1);
+        let addr_h = 0x32;
+        let addr_l = 0xDF;
+        memory.set(addr_l, zpg_addr.into());
+        memory.set(addr_h, zpg_addr.wrapping_add(1).into());
+        let addr = ((addr_h as u16) << 8 | addr_l as u16).wrapping_add(Y.into());
+        memory.set(0x42, addr);
+
+        memory.set(OPCODE_LDA_IDY, MEM_OFFSET + 2);
+        let zpg_addr = 0x57;
+        memory.set(zpg_addr, MEM_OFFSET + 3);
+        let addr_h = 0x83;
+        let addr_l = 0xCC;
+        memory.set(addr_l, zpg_addr.into());
+        memory.set(addr_h, zpg_addr.wrapping_add(1).into());
+        let addr = ((addr_h as u16) << 8 | addr_l as u16).wrapping_add(Y.into());
+        memory.set(0x00, addr);
+
+        memory.set(OPCODE_LDA_IDY, MEM_OFFSET + 4);
+        let zpg_addr = 0x69;
+        memory.set(zpg_addr, MEM_OFFSET + 5);
+        let addr_h = 0x77;
+        let addr_l = 0x86;
+        memory.set(addr_l, zpg_addr.into());
+        memory.set(addr_h, zpg_addr.wrapping_add(1).into());
+        let addr = ((addr_h as u16) << 8 | addr_l as u16).wrapping_add(Y.into());
+        memory.set(0x80, addr);
+
+        let mut cpu = CPU::new(memory);
+        cpu.reset();
+        cpu.y = Y;
 
         let init_pc = cpu.pc;
         let init_cycles = cpu.cycles;
